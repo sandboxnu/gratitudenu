ceb3a3fbdb1fe0a7c22cd829466a2fed1ecf8985
eliza <elizazijinhuang@gmail.com>
2021-02-18 10:07:33 -0500

    add controller

--- src/backend/src/form-responses/form-responses.controller.ts
+++ src/backend/src/form-responses/form-responses.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
+import { Question } from 'src/entities/question.entity';
+import { User } from 'src/entities/user.entity';
+import { FormResponse } from 'src/entities/formResponse.entity';
 
 @Controller('form-responses')
-export class FormResponsesController {}
+export class FormResponsesController {
+  @Post()
+  async create(
+    @Body('userId') userId: number,
+    @Body('questionId') questionId: number,
+    @Body('answer') answer: string,
+  ): Promise<FormResponse> {
+    const formResponse = new FormResponse();
+    const user = await User.findOne(userId);
+    const question = await Question.findOne(questionId);
+    if (!user || !question) {
+      throw new BadRequestException('Invalid userId or questionId');
+    }
+    formResponse.user = user;
+    formResponse.question = question;
+    await formResponse.save();
+    return formResponse;
+  }
+}
