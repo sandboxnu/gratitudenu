077cb5ed083f738a6b4f2e22f1b3a6bc6091b8a9
aislinblack <1of4black@gmail.com>
2021-02-17 19:55:34 -0500

    Add create method for users

--- src/backend/src/users/users.controller.ts
+++ src/backend/src/users/users.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Param, Post } from '@nestjs/common';
+import { User } from 'src/entities/user.entity';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Post()
+  create(
+    @Param('firstName') firstName: string,
+    @Param('lastName') lastName: string,
+    @Param('age') age: number,
+    @Param('consentFormFilled') consentFormFilled: boolean,
+  ): number {
+    const user = User.create({ firstName, lastName, age, consentFormFilled });
+    return user.id;
+  }
+}
