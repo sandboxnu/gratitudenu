e7276478d5ac0f23e4ed768f5123e71e37726de2
Zefeng (Daniel) Wang <wang.zef@husky.neu.edu>
2021-02-19 16:14:51 -0500

    fix async, create parameters.

--- src/backend/src/players/players.service.ts
+++ src/backend/src/players/players.service.ts
@@ -3,16 +3,17 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Player } from '../entities/player.entity';
 import { Repository } from 'typeorm';
 import { User } from '../entities/user.entity';
-import { Game } from '../entities/game.entity';
 
 @Injectable()
 export class PlayersService {
   constructor(
     @InjectRepository(Player)
     private playersRepository: Repository<Player>,
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
   ) {}
 
-  findAll(): Promise<Player[]> {
+  async findAll(): Promise<Player[]> {
     return this.playersRepository.find();
   }
 
@@ -25,14 +26,19 @@ export class PlayersService {
   }
 
   // Not sure if I should be taking these in or userId, gameId?
-  async create(user: User, game: Game, color: string): Promise<Player> {
+  async create(userId: number, gameId: number, color: string): Promise<Number> {
     let player = new Player();
-    player.user = user;
-    player.game = game; // Do I even want to set this?
     player.color = color;
+
+    const user = await this.usersRepository.findOne(userId);
+    if (!user) {
+      throw new BadRequestException('User not found');
+    }
+
+    // Set game by using gamesRepository.findOne() => does not exist
 
     await this.playersRepository.save(player);
 
-    return player;
+    return player.id;
   }
 }
