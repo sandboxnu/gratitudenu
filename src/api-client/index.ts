1424a5acc01dbe1725d34a6e2ed90256afe57b44
aislinblack <1of4black@gmail.com>
2021-03-07 14:23:13 -0500

    Create api-client

---
+++ src/api-client/index.ts
@@ -0,0 +1,46 @@
+import Axios, { AxiosInstance, Method } from 'axios';
+import { plainToClass } from 'class-transformer';
+import { ClassType } from 'class-transformer/ClassTransformer';
+
+// Return type of array item, if T is an array
+type ItemIfArray<T> = T extends (infer I)[] ? I : T;
+
+class APIClient {
+  private axios: AxiosInstance;
+
+  /**
+   * Send HTTP and return data, optionally serialized with class-transformer (helpful for Date serialization)
+   * @param method HTTP method
+   * @param url URL to send req to
+   * @param responseClass Class with class-transformer decorators to serialize response to
+   * @param body body to send with req
+   */
+  private async req<T>(
+    method: Method,
+    url: string,
+    responseClass?: ClassType<ItemIfArray<T>>,
+    body?: any,
+  ): Promise<T>;
+  private async req<T>(
+    method: Method,
+    url: string,
+    responseClass?: ClassType<T>,
+    body?: any,
+  ): Promise<T> {
+    const res = (await this.axios.request({ method, url, data: body })).data;
+    return responseClass ? plainToClass(responseClass, res) : res;
+  }
+
+  player = {
+    create: async (body: {
+      userId: number;
+      emotionId: number;
+    }): Promise<Number> => this.req('POST', '/players', Number, body),
+  };
+
+  constructor(baseURL = '') {
+    this.axios = Axios.create({ baseURL: baseURL });
+  }
+}
+
+export const API = new APIClient(process.env.NEXT_PUBLIC_API_URL);
