4ddd0dcdb526d5fd168f750bc4e8d4e15cda3fba
aislinblack <1of4black@gmail.com>
2021-03-08 20:15:44 -0500

    Add basic styles to page

--- src/frontend/pages/index.tsx
+++ src/frontend/pages/index.tsx
@@ -1,9 +1,26 @@
-import styles from '../styles/Home.module.css';
+import { ReactElement } from 'react';
+import styles from '../styles/Home.module.scss';
 
-export default function Login() {
+export default function Login(): ReactElement {
   return (
     <div className={styles.container}>
-      <div className="title">Behavior Game</div>
+      <div className={styles.title}>Behavior Game</div>
+      <div>
+        <div className={styles.formInput}>
+          <div>
+            User Id
+            <div className={styles.form}>
+              <input placeholder="Enter Input" />
+            </div>
+          </div>
+          <div>
+            Emotion Id
+            <div className={styles.form}>
+              <input placeholder="Enter Input" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
