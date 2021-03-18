b32a7f4e56932ff9f8d1a27524204bfcdd877fca
aislinblack <1of4black@gmail.com>
2021-03-18 16:28:46 -0400

    Make at least one helper for this mess

--- src/backend/src/export/export.controller.ts
+++ src/backend/src/export/export.controller.ts
@@ -44,43 +44,46 @@ export class ExportController {
       ),
     );
 
-    gamesWithRelations.forEach((game) => {
-      data.push({
-        game: `Game: ${game.id}`,
-        emotion: `Emotion: ${game.players[0].emotionId}`,
-      });
-      const p1 = game.players[0];
-      const p2 = game.players[1];
-      const p3 = game.players[2];
-      const p4 = game.players[3];
-
-      game.rounds.forEach((round) => {
-        const p1Move = p1.grabs.find((grab) => grab.round.id === round.id);
-        const p2Move = p2.grabs.find((grab) => grab.round.id === round.id);
-        const p3Move = p3.grabs.find((grab) => grab.round.id === round.id);
-        const p4Move = p4.grabs.find((grab) => grab.round.id === round.id);
-        data.push({
-          round: round.roundNumber,
-          playerOne: p1.userId,
-          playerOneTake: p1Move.howMany,
-          playerOneTime: p1Move.timeTaken,
-          playerTwo: p2.userId,
-          playerTwoTake: p2Move.howMany,
-          playerTwoTime: p2Move.timeTaken,
-          playerThree: p3.userId,
-          playerThreeTake: p3Move.howMany,
-          playerThreeTime: p3Move.timeTaken,
-          playerFour: p4.userId,
-          playerFourTake: p4Move.howMany,
-          playerFourTime: p4Move.timeTaken,
-        });
-      });
-    });
+    gamesWithRelations.forEach((game) => this.formatGameIntoData(data, game));
 
     const csv = Papa.unparse(data, {
       columns: COLUMNS,
     });
 
     return csv;
+  }
+
+  private formatGameIntoData(data, game: Game) {
+    data.push({
+      game: `Game: ${game.id}`,
+      emotion: `Emotion: ${game.players[0].emotionId}`,
+    });
+    const p1 = game.players[0];
+    const p2 = game.players[1];
+    const p3 = game.players[2];
+    const p4 = game.players[3];
+
+    game.rounds.forEach((round) => {
+      const p1Move = p1.grabs.find((grab) => grab.round.id === round.id);
+      const p2Move = p2.grabs.find((grab) => grab.round.id === round.id);
+      const p3Move = p3.grabs.find((grab) => grab.round.id === round.id);
+      const p4Move = p4.grabs.find((grab) => grab.round.id === round.id);
+      data.push({
+        round: round.roundNumber,
+        playerOne: p1.userId,
+        playerOneTake: p1Move.howMany,
+        playerOneTime: p1Move.timeTaken,
+        playerTwo: p2.userId,
+        playerTwoTake: p2Move.howMany,
+        playerTwoTime: p2Move.timeTaken,
+        playerThree: p3.userId,
+        playerThreeTake: p3Move.howMany,
+        playerThreeTime: p3Move.timeTaken,
+        playerFour: p4.userId,
+        playerFourTake: p4Move.howMany,
+        playerFourTime: p4Move.timeTaken,
+      });
+    });
+    data.push({}); // new line in between
   }
 }
