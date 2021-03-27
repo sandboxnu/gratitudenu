8307bf359e462381cb0a8272b74c01926d70a675
aislinblack <1of4black@gmail.com>
2021-03-27 13:22:35 -0400

    remove console

--- src/frontend/hooks/useSetting.ts
+++ src/frontend/hooks/useSetting.ts
@@ -4,11 +4,10 @@ import { API } from '../api-client';
 type Hook = (settingName: string, defaultValue: number) => number;
 
 export const useSetting: Hook = (settingName: string, defaultValue: number) => {
-  const { data, error } = useSWR(
-    `/admin?settingName=${settingName}`,
-    async () => API.settings.get(settingName),
+  const { data } = useSWR(`/admin?settingName=${settingName}`, async () =>
+    API.settings.get(settingName),
   );
-  console.log(data);
+
   if (data) {
     return data.valueOf();
   }
