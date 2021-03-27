f18269f59c29299bc84b44fb1a1c0f1f4ff33dc1
aislinblack <1of4black@gmail.com>
2021-03-27 12:47:26 -0400

    Use setting value instead of constant

--- src/backend/src/game/game.service.ts
+++ src/backend/src/game/game.service.ts
@@ -5,6 +5,7 @@ import { Repository } from 'typeorm';
 import { Game } from '../entities/game.entity';
 import { Round } from '../entities/round.entity';
 import { Grab } from 'src/entities/grab.entity';
+import { Setting } from 'src/entities/setting.entity';
 
 const MAX_POINTS = 200;
 const MAX_ROUND_COUNT = 10;
@@ -12,6 +13,8 @@ const MAX_ROUND_COUNT = 10;
 type GameRoundID = {
   gameId: number;
 };
+
+const ROUND_SETTING_NAME = 'ROUND';
 
 @Injectable()
 export class GameService {
@@ -32,12 +35,16 @@ export class GameService {
       }),
     );
 
+    const maxRounds =
+      (await Setting.findOne(ROUND_SETTING_NAME)).value || MAX_ROUND_COUNT;
+
     const game = await Game.create({
       ongoing: true,
       players,
+      maxRounds,
     }).save();
 
-    const newRound = await Round.create({
+    await Round.create({
       roundNumber: 1,
       pointsRemaining: MAX_POINTS,
       game,
@@ -74,7 +81,7 @@ export class GameService {
     // no points remaining, or max round
     const pointsRemaining = await this.getSumPoints(roundId);
     const roundCount = game.rounds.length;
-    if (pointsRemaining <= 0 || roundCount >= MAX_ROUND_COUNT) {
+    if (pointsRemaining <= 0 || roundCount >= game.maxRounds) {
       game.ongoing = false;
       await game.save();
     }
